ae057190e212705b9cf66c7a8f13026fb7baf2a2
Sergey Orlovskyi <blackoutxray@gmail.com>
2019-06-15 12:54:34 +0300

    chaged 4th lab

--- Practice labs option 17 year2 term2/17_Pract_4/17_Pract_4_code/lineequation.cpp
+++ Practice labs option 17 year2 term2/17_Pract_4/17_Pract_4_code/lineequation.cpp
@@ -42,42 +42,20 @@ float LineEquation::operator%(const LineEquation &line)
     Point firstPoint1 = findIntersectionWithOx();
     Point firstPoint2 = findIntersectionWithOy();
 
-    if (firstPoint1.x <= SMALL_FLOAT && firstPoint1.x >= -SMALL_FLOAT &&
-            firstPoint1.y <= SMALL_FLOAT && firstPoint1.y >= -SMALL_FLOAT)
-    {
-        firstPoint1.x = FIRST_CONST_IF_SMTH_GOES_WRONG;
-        firstPoint1.y = k*FIRST_CONST_IF_SMTH_GOES_WRONG + b;
-    }
-    if (firstPoint2.x <= SMALL_FLOAT && firstPoint2.x >= -SMALL_FLOAT &&
-            firstPoint2.y <= SMALL_FLOAT && firstPoint2.y >= -SMALL_FLOAT)
-    {
-        firstPoint2.x = SECOND_CONST_IF_SMTH_GOES_WRONG;
-        firstPoint2.y = k*SECOND_CONST_IF_SMTH_GOES_WRONG + b;
-    }
+    processPoints(firstPoint1, firstPoint2, k, b);
 
     Point secondPoint1 = line.findIntersectionWithOx();
     Point secondPoint2 = line.findIntersectionWithOy();
 
-    if (secondPoint1.x <= SMALL_FLOAT && secondPoint1.x >= -SMALL_FLOAT &&
-            secondPoint1.y <= SMALL_FLOAT && secondPoint1.y >= -SMALL_FLOAT)
-    {
-        secondPoint1.x = FIRST_CONST_IF_SMTH_GOES_WRONG;
-        secondPoint1.y = line.k*FIRST_CONST_IF_SMTH_GOES_WRONG + line.b;
-    }
-    if (secondPoint2.x <= SMALL_FLOAT && secondPoint2.x >= -SMALL_FLOAT &&
-            secondPoint2.y <= SMALL_FLOAT && secondPoint2.y >= -SMALL_FLOAT)
-    {
-        secondPoint2.x = SECOND_CONST_IF_SMTH_GOES_WRONG;
-        secondPoint2.y = line.k*SECOND_CONST_IF_SMTH_GOES_WRONG + line.b;
-    }
+    processPoints(secondPoint1, secondPoint2, line.k, line.b);
 
     Point vec1 = {firstPoint2.x - firstPoint1.x, firstPoint2.y - firstPoint1.y};
     Point vec2 = {secondPoint2.x - secondPoint1.x, secondPoint2.y - secondPoint1.y};
 
 
-    float one = fabs(vec1.x * vec2.x + vec1.y * vec2.y);
-    float two = sqrt(vec1.x * vec1.x + vec1.y * vec1.y);
-    float three = sqrt(vec2.x * vec2.x + vec2.y * vec2.y);
+    // float one = fabs(vec1.x * vec2.x + vec1.y * vec2.y);
+    // float two = sqrt(vec1.x * vec1.x + vec1.y * vec1.y);
+    // float three = sqrt(vec2.x * vec2.x + vec2.y * vec2.y);
 
     float angle = fabs(vec1.x * vec2.x + vec1.y * vec2.y)/(sqrt(vec1.x * vec1.x + vec1.y * vec1.y)
                                                            *sqrt(vec2.x * vec2.x + vec2.y * vec2.y));
@@ -85,6 +63,22 @@ float LineEquation::operator%(const LineEquation &line)
     angle = acos(angle);
 
     return angle;
+}
+
+void processPoints(Point &point1, Point &point2, float k, float b)
+{
+    if (point1.x <= SMALL_FLOAT && point1.x >= -SMALL_FLOAT &&
+            point1.y <= SMALL_FLOAT && point1.y >= -SMALL_FLOAT)
+    {
+        point1.x = FIRST_CONST_IF_SMTH_GOES_WRONG;
+        point1.y = k*FIRST_CONST_IF_SMTH_GOES_WRONG + b;
+    }
+    if (point2.x <= SMALL_FLOAT && point2.x >= -SMALL_FLOAT &&
+            point2.y <= SMALL_FLOAT && point2.y >= -SMALL_FLOAT)
+    {
+        point2.x = SECOND_CONST_IF_SMTH_GOES_WRONG;
+        point2.y = k*SECOND_CONST_IF_SMTH_GOES_WRONG + b;
+    }
 }
 
 bool LineEquation::operator&&(const LineEquation &line)
